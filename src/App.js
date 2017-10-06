605b388ab17b2abda566036728f75060c55f6042
Caleb Bassham <942cb05@students.olatheschools.com>
2017-10-06 21:08:04 +0000

    Added the first 7 pictures and captions

--- src/App.js
+++ src/App.js
@@ -8,59 +8,37 @@ class App extends Component {
   render() {
     const frames = [
       {
-        "title": "Lynn",
-        "body": "Fugiat dolor minim excepteur voluptate enim nulla duis Lorem magna esse incididunt cupidatat ex. Culpa enim esse magna duis eiusmod Lorem ex ea sint veniam. Cillum aute deserunt pariatur occaecat deserunt in aliquip veniam cillum ad ea.",
-        image: <Image src='https://placehold.it/500x500'></Image>
+        "body": "In the Civil War firefighters started by putting out fires. At this time houses were not fire-proofed. Books started to build up due to the explosion of ideas.",
+        image: <Image src='https://i.pinimg.com/originals/1f/c3/be/1fc3be8f523e1b3455e387e61463f60b.jpg'></Image>
       },
       {
-        "title": "Benson",
-        "body": "Est ipsum adipisicing mollit deserunt occaecat do ipsum consectetur aliqua voluptate adipisicing. Ex anim ex ex nostrud cupidatat sunt anim fugiat et. Exercitation qui magna tempor eiusmod quis pariatur cupidatat sint qui velit nostrud commodo excepteur sint.",
-        image: <Image src='https://placehold.it/500x500'></Image>
+        body: "Technology advances and becomes a part of everyday life. Houses are fire-proofed so firefighters are not needed for the original purpose.",
+        image: <Image src='http://2.bp.blogspot.com/-ZVlHn-sVMh0/U65FwcqNlyI/AAAAAAAAFPg/PJsCwGsFw-Q/s1600/evolution-phone.jpg'></Image>
       },
       {
-        "title": "Goff",
-        "body": "Aliquip id nulla quis velit occaecat ad. Amet do eu dolore voluptate adipisicing adipisicing adipisicing. Magna ad ut dolore quis tempor do.",
-        image: <Image src='https://placehold.it/500x500'></Image>
+        "body": "Books begin to be dimmed down so the different minorites wouldn't be upset. The people who did not agree with the changes waited too long before speaking out.",
+        image: <Image src='http://i2.wp.com/www.rightattitudes.com/blogincludes/images/How_to_Read_a_Pile_of_Books.jpg'></Image>
       },
       {
-        "title": "Levy",
-        "body": "Occaecat nostrud aliqua occaecat incididunt deserunt nostrud excepteur dolor reprehenderit esse adipisicing. Sint exercitation culpa ut culpa ut. Deserunt magna commodo duis sit.",
-        image: <Image src='https://placehold.it/500x500'></Image>
+        body: "As society began to move into a utopian society, films are sped up and school days are shortened. Books are condensed to paragraphs or sentences.",
+        image: <Image src='http://utopiafailure.weebly.com/uploads/1/5/3/6/15360626/899147806.jpg'></Image>
       },
       {
-        "title": "Serrano",
-        "body": "Do esse quis consequat proident. Culpa esse proident excepteur sit et tempor id id labore deserunt laborum officia do. Excepteur qui labore minim laborum Lorem cillum dolore deserunt ad proident.",
-        image: <Image src='https://placehold.it/500x500'></Image>
+        body: "Technology takes over every day life. People get lazy and become antisocial due to technology. They did whatever they wanted in order to be happy.",
+        image: <Image src='http://media02.hongkiat.com/cities-of-the-futures/futurecitytoorobertdbrown.jpg'></Image>
       },
       {
-        "title": "Charlene",
-        "body": "Do id commodo deserunt excepteur consectetur enim do anim veniam cillum reprehenderit cillum adipisicing. Officia reprehenderit ex ad aliquip enim excepteur sint deserunt velit nisi laboris amet. Aliquip laboris sit ipsum incididunt nulla non est labore ea irure.",
-        image: <Image src='https://placehold.it/500x500'></Image>
+        body: "Because critics justify getting rid of books, firemen are repurposed to burn books because they are now illegal to be read.",
+        image: <Image src='http://www.teemingbrain.com/wp-content/uploads/2012/09/fahrenheit-451-burning-books.jpg'></Image>
       },
       {
-        "title": "Klein",
-        "body": "Voluptate mollit magna laborum voluptate ullamco minim est commodo ea minim enim nulla. In nisi aliquip ex sint cupidatat ea consequat cillum laborum quis sit magna in minim. Est mollit consectetur tempor consectetur proident anim ea tempor reprehenderit sit adipisicing fugiat id sint.",
-        image: <Image src='https://placehold.it/500x500'></Image>
-      },
-      {
-        "title": "Hunt",
-        "body": "Adipisicing minim fugiat ullamco commodo sint duis cupidatat irure consequat veniam aute excepteur ex laborum. Adipisicing officia cillum in ullamco consectetur enim incididunt reprehenderit dolor nostrud nostrud pariatur. Minim cupidatat quis aliqua voluptate nulla cillum occaecat nostrud ullamco dolor.",
-        image: <Image src='https://placehold.it/500x500'></Image>
-      },
-      {
-        "title": "Hopper",
-        "body": "Nulla cillum ut elit enim elit nisi velit. Mollit magna in adipisicing aliquip nisi ea est do cupidatat qui. Sunt sit eiusmod aliqua occaecat aliqua ullamco ea ea eu voluptate velit aliqua reprehenderit minim.",
-        image: <Image src='https://placehold.it/500x500'></Image>
-      },
-      {
-        "title": "Tanisha",
-        "body": "Exercitation ad commodo nostrud mollit labore non sit. Ut occaecat consequat fugiat ea. Labore enim deserunt nulla tempor tempor tempor commodo nostrud.",
-        image: <Image src='https://placehold.it/500x500'></Image>
+        body: "People comply with the law and don't read books because they fear the firemen. The government fears people reading because they don't want to lose power.",
+        image: <Image src='https://i.ytimg.com/vi/p97kSsORsDE/maxresdefault.jpg'></Image>
       }
     ]
 
     return (
-      <div className="ui container">
+      <div className="ui container" style={{marginTop: '1em'}}>
         <Grid columns='equal'>
           {frames.map((frame, index) => {
             return (
